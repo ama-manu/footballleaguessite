fef6dcb83551b871d24c24dd41a5f3e7c0bc18d7
ama-manu <ama.manu@proton.me>
2023-10-18 19:30:04 +0200

    fix issues with data calculations

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,9 @@ function calcPosChange(tables) {
         tables[i].forEach(team => {
             const teamPreviousDay = tables[i - 1].find(x => x.teamName === team.teamName);
             if (team.position > teamPreviousDay.position) {
-                team.posChange = "up";
-            } else if (team.position < teamPreviousDay.position) {
                 team.posChange = "down";
+            } else if (team.position < teamPreviousDay.position) {
+                team.posChange = "up";
             }
         });
     }
@@ -91,6 +91,11 @@ function calcLeague(matches) {
         const pointsTeam1 = match.matchResults[1].pointsTeam1;
         const pointsTeam2 = match.matchResults[1].pointsTeam2;
 
+
+        // sort goals by time
+        var sortedGoals = match.goals.sort((a, b) => {
+            return a.matchMinute - b.matchMinute;
+        });
         var goals = match.goals.at(-1);
         // if no goals, add 0:0
         if (goals === undefined) {
@@ -98,6 +103,10 @@ function calcLeague(matches) {
                 "scoreTeam1": 0,
                 "scoreTeam2": 0
             }
+        }
+
+        if ((match.team1.teamName === "Bayer Leverkusen" || match.team2.teamName === "Bayer Leverkusen") && match.group.groupOrderID === 5) {
+            console.log(match);
         }
 
         // construct teams
@@ -108,12 +117,16 @@ function calcLeague(matches) {
         if (pointsTeam1 > pointsTeam2) {
             newTeam1.points = 3;
             newTeam1.won = 1;
+            newTeam2.lost = 1;
         } else if (pointsTeam1 < pointsTeam2) {
+            newTeam1.lost = 1;
             newTeam2.points = 3;
             newTeam2.won = 1;
         } else {
-            newTeam1.points, newTeam2.points = 1;
-            newTeam1.draw, newTeam2.draw = 1;
+            newTeam1.points = 1;
+            newTeam2.points = 1;
+            newTeam1.draw = 1;
+            newTeam2.draw = 1;
         }
 
 
@@ -123,23 +136,25 @@ function calcLeague(matches) {
 
     });
 
-    // add up points
+    // add up stats
     // select table, first matchday doesnt need adding up
     for (i = 1; i < tables.length; i++) {
         // select team
         tables[i].forEach(team => {
-            // add points from previous matchday
-            tables[i - 1].forEach(teamPre => {
-                if ((team.teamName === teamPre.teamName) && (team.matches == (teamPre.matches + 1))) {
-                    team.points += teamPre.points;
-                    team.opponentGoals += teamPre.opponentGoals;
-                    team.goals += teamPre.goals;
-                    team.won += teamPre.won;
-                    team.lost += teamPre.lost;
-                    team.draw += teamPre.draw;
-                    team.goalDiff = team.goals - team.opponentGoals;
-                }
-            });
+            // add up stats from previous matchday
+            const teamPre = tables[i - 1].find(x => (x.teamName === team.teamName) && (team.matches === (x.matches + 1)));
+            if ((team.teamName === teamPre.teamName) && (team.matches === (teamPre.matches + 1))) {
+                team.points += teamPre.points;
+                team.opponentGoals += teamPre.opponentGoals;
+                team.goals += teamPre.goals;
+                team.won += teamPre.won;
+                team.lost += teamPre.lost;
+                team.draw += teamPre.draw;
+                team.goalDiff = team.goals - team.opponentGoals;
+
+
+
+            }
         });
     }
 
