90d56f592aa797871ea1346085c4b579fea491a4
ama-manu <ama.manu@proton.me>
2024-03-13 13:16:06 +0100

    add basic league card for landing page

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 import styles from './Card.module.scss'
 
-function Card({children}) {
+function Card({ countryURL, league }) {
   return (
-    <div className={styles.card}>
-        {children}
-    </div>
+    // <div className={styles.card}>
+      <Link className={styles.title} to={`/${countryURL}/${league.internalURL}`}>
+        <img className={styles.img} src={league.logoURL}></img>
+        {/* {league.name} */}
+      </Link>
+    // </div>
   )
 }
 
