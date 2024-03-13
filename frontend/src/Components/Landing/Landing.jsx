b32e9ce9fa6ae8732cc349dfa43d4ea435ca66d8
ama-manu <ama.manu@proton.me>
2024-03-13 13:16:19 +0100

    add basic landing page

--- frontend/src/Components/Landing/Landing.jsx
+++ frontend/src/Components/Landing/Landing.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-function Landing() {
+import styles from './Landing.module.scss'
+
+import Card from '../Card/Card.jsx'
+
+function Landing({ leagues }) {
   return (
-    <div>different football leagues</div>
+    // <Card countryURL={leagues[0].internalURL} league={leagues[0].submenu[0]}></Card>
+    <div className={styles.country}>
+      {leagues.map((league, index) => (
+        <div key={index}>
+          <h1>{league.country}</h1>
+          <ul className={styles.cardList}>
+            {league.submenu.map((item, subIndex) => (
+              <li key={subIndex}>
+                <Card countryURL={league.internalURL} league={item}></Card>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+
   )
 }
 
