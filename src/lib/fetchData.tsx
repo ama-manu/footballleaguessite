de4eebfbe72b2fbf30d790c06338aeef0aeb7415
ama-manu <ama.manu@proton.me>
2025-07-29 01:07:23 +0200

    add code to possibly use swr to fetch data

--- src/lib/fetchData.tsx
+++ src/lib/fetchData.tsx
@@ -1,13 +1,25 @@
+// "use client";
+// import useSWR from "swr";
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 async function fetchData(url: string) {
-	try {
-		const response = await fetch(url);
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
+	// using fetch()
+		try {
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			return await response.json();
+		} catch (error) {
+			console.error("Fetch error:", error);
+			return null;
 		}
-		return await response.json();
-	} catch (error) {
-		console.error("Fetch error:", error);
-		return null;
 	}
-}
+
+// function fetchData(url: string) {
+// 	const { data, error, isLoading } = useSWR(url, fetcher);
+// 	return { data, error, isLoading };
+// }
+
 export default fetchData;
